4693e7dde0592512842a59e024d3ca58a6afefbe
yukiharada1228 <yukiharada1228@gmail.com>
2026-01-04 10:07:25 +0900

    feat: add LinearGate and CorrectGate to default DCL search gates

--- examples/CIFAR100/dcl_search.py
+++ examples/CIFAR100/dcl_search.py
@@ -27,7 +27,7 @@ parser.add_argument(
 )
 parser.add_argument(
     "--gates",
-    default=["ThroughGate", "CutoffGate"],
+    default=["ThroughGate", "CutoffGate", "LinearGate", "CorrectGate"],
     nargs="*",
     type=str,
 )
