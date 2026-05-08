031807a7fee094a4693436b40dee7463853d79cf
yukiharada1228 <yukiharada1228@gmail.com>
2026-05-08 02:35:33 +0000

    This version of Antigravity is no longer supported. Please upgrade to receive the latest features.

--- examples/CIFAR10/SimCLR/models/cifar_models/__init__.py
+++ examples/CIFAR10/SimCLR/models/cifar_models/__init__.py
@@ -1,5 +1,14 @@
-from .cifar_resnet import resnet50
+from .cifar_resnet import (resnet20, resnet32, resnet44, resnet56, resnet110,
+                           resnet1202, resnet50)
+from .cifar_wideresnet import wideresnet28_2
 
 __all__ = [
+    "resnet20",
+    "resnet32",
+    "resnet44",
+    "resnet56",
+    "resnet110",
+    "resnet1202",
     "resnet50",
+    "wideresnet28_2",
 ]
